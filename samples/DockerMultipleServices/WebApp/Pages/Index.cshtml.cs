f3d82fa7d93588413194aeddd5a76036c78f42b9
glennc <glennc@microsoft.com>
2017-11-11 08:47:47 -0800

    commit initial sample

---
+++ samples/DockerMultipleServices/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
+
+namespace ServiceB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Data;
+        private IHttpClientFactory _factory;
+
+        public IndexModel(IHttpClientFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task OnGet()
+        {
+            var client = _factory.CreateClient("service");
+            Data = await client.GetStringAsync("api/values");
+        }
+    }
+}
